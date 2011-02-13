9deebe8984f9879f13ebd19bd48f8197a1f89523
Florian Hanke <florian.hanke@gmail.com>
2011-02-13 14:56:42 +0100

    + speed spec help for other committers :)

--- spec/spec_helper_extensions.rb
+++ spec/spec_helper_extensions.rb
@@ -3,11 +3,23 @@ module SpecHelperExtensions
   # performance_of { do something here }.should < 0.0001
   #
   require 'benchmark'
+  # Try to load the Phony::PerformanceRatio from
+  # the file spec/performance_ratio.rb - set it to the
+  # ratio you think it is compared to my MacBook 2.66GHz i7.
+  #
+  # Example:
+  #   Phony::PerformanceRatio = 0.8 # for 80% of my MacBook speed.
+  #
+  begin
+    require File.expand_path '../performance_ratio', __FILE__
+  rescue LoadError => e
+    Phony::PerformanceRatio = 1
+  end
   def performance_of &block
     GC.disable
     result = Benchmark.realtime &block
     GC.enable
-    result
+    result * Phony::PerformanceRatio
   end
   
   # Allows the definition of "describe-wide" examples and describes.
