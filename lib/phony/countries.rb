b6cc8621e01e7593b2d3406c9ac2109448254322
Chengchen JI <jichengchen1988@gmail.com>
2013-10-18 12:16:14 +0200

    new implementation

--- lib/phony/countries.rb
+++ lib/phony/countries.rb
@@ -662,12 +662,10 @@ Phony.define do
 
   # Iraq (Republic of)
   country '964',
-          one_of('1')                                  >> split(3,4) | # Baghdad
-          one_of('21', '23', '24', '25', '30', '32',
-                 '33', '36', '37', '40', '42', '43',
-                 '50', '53', '60', '62', '66')         >> split(3,3) | # other cities
-          match(/^(7[3-9]\d)\d+$/)                     >> split(3,4),  # mobiles
-          length( 1 => 8, 2 => 8, 3 => 10 )
+          one_of(%w(21 23 24 25 30 32 33 36 37 40 42 43 50 53 60 62 66)) >> split(3,3) |
+          one_of(%w(71 72 73 74 75 76 77 78 79))                         >> split(4,4) | # mobile
+          one_of('1')                                                    >> split(3,4) |
+          fixed(2)                                                       >> split(3,3)
 
   country '965', todo # Kuwait (State of)
   country '966', todo # Saudi Arabia (Kingdom of)
