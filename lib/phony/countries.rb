036a729e76f763910d52c9554ab00a0767246dc0
Alvaro Fussen <github@o08.net>
2013-11-05 14:03:44 +0100

    iraq: allow length 8..11

--- lib/phony/countries.rb
+++ lib/phony/countries.rb
@@ -666,7 +666,7 @@ Phony.define do
           one_of(%w(21 23 24 25 30 32 33 36 37 40 42 43 50 53 60 62 66)) >> split(3,3) |
           match(/^(7[1-9]\d)\d+$/)                                       >> split(3,4) |
           match(/^(\d{2})\d+$/)                                          >> split(3,3),  # catchall
-          length( 1 => 8, 2 => 8, 3 => 10..11)
+          length(8..11)
 
   country '965', todo # Kuwait (State of)
   country '966', todo # Saudi Arabia (Kingdom of)
