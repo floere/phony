e851df82c81e8839e803c023722f4665162d4407
Marc Boquet <marc@apalan.cat>
2014-08-28 17:19:03 +0200

    Remove Spain :local specs for now

--- spec/lib/phony/country_codes_spec.rb
+++ spec/lib/phony/country_codes_spec.rb
@@ -72,16 +72,6 @@ describe Phony::CountryCodes do
       it 'formats spain correctly' do
         @countries.formatted("34123456789", :format => :national).should eql '123 456 789'
       end
-      it 'formats landline numbers correctly with :local' do
-        # TODO local numbers don't work anymore in spain, area code is necessary.
-        @countries.formatted("34910000000", :format => :local).should eql '000 00 00'
-      end
-      it 'formats spain special numbers correctly with :local' do
-        @countries.formatted("34900000000", :format => :local).should eql '900 000 000'
-      end
-      it 'formats spain mobile numbers correctly with :local' do
-        @countries.formatted("34600000000", :format => :local).should eql '600 000 000'
-      end
     end
     context 'default' do
       it "should format swiss numbers" do
