83c96c95046adc59c81bf9008eae49c0d15286f8
Florian R. Hanke <florian.hanke@gmail.com>
2015-04-15 13:08:30 +0200

    Add default specs.

---
+++ spec/lib/phony/national_splitters/default_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Phony::NationalSplitters::Default do
+  
+  describe 'instance_for' do
+    it 'caches' do
+      described_class.instance_for.should equal(described_class.instance_for)
+    end
+  end
+  
+  context 'with instance' do
+    let(:splitter) { described_class.instance_for }
+    
+    describe 'split' do
+      it 'does not split' do
+        splitter.split(:anything).should == [:anything]
+      end
+    end
+    
+    describe 'plausible?' do
+      it 'is always plausible' do
+        splitter.plausible?(:anything, :anything).should be_true
+      end
+    end
+    
+    describe 'length' do
+      it 'needs to be at least 3' do
+        splitter.length.should == 3
+      end
+    end
+    
+  end
+  
+end
