6dd67a367665cbe30a990d3d6febee4cca685a87
Florian R. Hanke <florian.hanke@gmail.com>
2015-04-22 08:08:38 +0200

    More docs.

--- lib/phony/national_splitters/default.rb
+++ lib/phony/national_splitters/default.rb
@@ -10,10 +10,30 @@ module Phony
         @instance ||= new
       end
 
+      # "Splits" the national part of a phone number into a single piece.
+      #
+      # @param [String] national_number An national part of a number.
+      #
+      # @return [Array<String>] An Array with the given number part as its element.
+      #
+      # @example Split the national part of a Swiss number.
+      #   Phony.split("1234567") # => ["1234567"]
+      #
       def split national_number
         [national_number]
       end
       
+      # By default, the national part of a number is always plausible.
+      #
+      # @param [String] rest An national part of a number (ignored).
+      # @param [Fixnum] size Size (ignored).
+      # @param [Hash] hints Hints (ignored).
+      #
+      # @return [Boolean] Always true.
+      #
+      # @example Split the national part of a Swiss number.
+      #   Phony.plausible?("1234567") # => true
+      #
       def plausible? rest, size, hints = {}
         true
       end
