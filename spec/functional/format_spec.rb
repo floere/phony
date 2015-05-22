aed5e21fa55c845ea63ea4567529b1f665b4498a
Florian R. Hanke <florian.hanke@gmail.com>
2015-05-22 20:57:53 +0200

    Add test docs.

--- spec/functional/format_spec.rb
+++ spec/functional/format_spec.rb
@@ -92,16 +92,14 @@ describe 'Phony#format' do
       it { Phony.format('18705551122').should eql '+1 (870) 555-1122' }
       it { Phony.format('18091231234', :format => :international).should eql '+1 (809) 123-1234' }
       it { Phony.format('18091231234', :format => :international, :spaces => '').should eql '+1(809)123-1234' }
-      # Gets a trunk code.
-      it { Phony.format('14152223333', :format => :national).should eql '(415) 222-3333' }
-      # Does not show a trunk code.
-      it { Phony.format('14152223333', :format => :national).should eql '(415) 222-3333' }
       it { Phony.format('18091231234', :format => :international, :spaces => :-).should eql '+1-(809)-123-1234' }
+      # Does not show a trunk code by default on national level.
+      it { Phony.format('14152223333', :format => :national).should eql '(415) 222-3333' }
       it { Phony.format('14159224711', :format => :national).should eql '(415) 922-4711' }
-      # With forced trunk.
+      # With forced trunk option.
       it { Phony.format('14159224711', :format => :national, :trunk => true).should eql '1 (415) 922-4711' }
       it { Phony.format('14159224711', :format => :national, :trunk => false).should eql '(415) 922-4711' }
-      # With forced parentheses.
+      # With forced parentheses option.
       it { Phony.format('14159224711', :format => :national, :parentheses => true).should eql '(415) 922-4711' }
       it { Phony.format('14159224711', :format => :national, :parentheses => false).should eql '415 922-4711' }
     end
