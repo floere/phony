4b85a8ccd305e5cf29721da7bb643f7f2634aa6f
Florian R. Hanke <florian.hanke@gmail.com>
2015-08-16 16:30:15 +0200

    Add an issue snippet for 270.

---
+++ misc/issue-snippets/270.rb
@@ -0,0 +1,16 @@
+require 'phony'
+
+numbers = %w[
+  225784367
+  64225784367
+  +64225784367
+  0064225784367
+  0225784367
+]
+
+result = numbers.map do |number|
+  number = number.sub(/^[0\+]*(64+|\b)/, '64')
+  Phony.normalize(number)
+end
+
+puts result
