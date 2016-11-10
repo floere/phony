4174c80e8d989abf4bf610b057f949dfed4f0b94
Wagner Narde <wnarde@gmail.com>
2016-11-10 11:00:04 -0200

    Brings back mobile numbers spec.

--- spec/lib/phony/countries_spec.rb
+++ spec/lib/phony/countries_spec.rb
@@ -145,6 +145,23 @@ describe 'country descriptions' do
       it_splits '5547991311234', ['55', '47', '99131', '1234'] # Santa Catarina's 9th digit
       it_splits '5541991311234', ['55', '41', '99131', '1234'] # Parana's 9th digit
 
+      context 'mobile numbers' do
+        %w{
+          11 12 13 14 15 16 17 18 19
+          21 22 24 27 28
+          31 32 33 34 35 37 38
+          61 62 63 64 65 66 67 68 69
+          71 73 74 75 77 79
+          81 82 83 84 85 86 87 88 89
+          91 92 93 94 95 96 97 98 99
+          41 42 43 44 45 46
+          47 48 49
+          51 53 54 55
+        }.each do |state_code|
+          it_splits "55#{state_code}993051123", ['55', state_code, '99305', '1123']
+        end
+      end
+
       context "special numbers" do
         it_splits '5508002221234', ['55', '0800', '222', '1234']
         it_splits '5530032221', ['55', '3003', '2221']
