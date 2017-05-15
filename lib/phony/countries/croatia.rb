87a3503c13c1c054783b01031d2e8c88a054d1d4
Maciej Lopatka <gmsaner@gmail.com>
2017-05-15 15:02:59 +0200

    Update Croatia landline, mobile, premium and toll free format

--- lib/phony/countries/croatia.rb
+++ lib/phony/countries/croatia.rb
@@ -3,24 +3,19 @@
 # http://en.wikipedia.org/wiki/Telephone_numbers_in_Croatia
 # http://dialcode.org/Europe/Croatia/
 # http://www.howtocallabroad.com/croatia/
-#
-# Landline numbers are composed by a 2 digits area code + 6 fixed digits 
-# Mobile numbers are composed by a 2 digits prefix + 7 fixed digits 
+# https://github.com/googlei18n/libphonenumber/
 #
 
-mobile = %w(
-  91
-  92
-  95
-  97
-  977
-  98
-  99
-)
+landline = /^(2[0-3]|3[1-5]|4[02-47-9]|5[1-3])/
 
 Phony.define do
   country '385', trunk('0') |
-                 one_of(mobile) >> split(3, 4) |
-                 one_of('1')    >> split(4, 3) | # Zagreb
-                 fixed(2)       >> split(3, 3)   # 2-digit NDCs
+                 one_of('1')         >> split(4, 3)       | # Zagreb
+                 match(/^(80[01])/)  >> matched_split(
+                                    /\A\d{4}\z/ => [4],
+                                    /\A\d{6}\z/ => [3,3]) | # Toll free
+                 match(/^(9\d)/)     >> split(3, 3..4)    | # Mobile
+                 match(landline)     >> split(3, 3..4)    | # Landline
+                 match(/^(6[01])/)   >> split(2,2..3)     | # Premium rate
+                 match(/^([67]\d)/)  >> split(3,3..4)       # Premium, personal and UAN
 end
