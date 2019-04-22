53a0f3154d459455a482d0ceba3d11060977c3ba
Sergey Smirnov <sergey.smirnov@limebike.com>
2019-04-22 13:53:15 -0700

    added another case

--- lib/phony/countries.rb
+++ lib/phony/countries.rb
@@ -1183,9 +1183,9 @@ Phony.define do
   # Israel (State of) (also works for numbers in Palestinian territories)
   country '972',
     trunk('0') |
-    one_of('1')                            >> split(3,3,3) | # special numbers
-    one_of('2', '3', '4', '8', '9')        >> split(3,4)   | # 1 digit ndc
-    match(/^(5[012345689]|7[23467])\d+$/)  >> split(3,4)     # 2 digit ndc
+    one_of('1')                                 >> split(3,3,3) | # special numbers
+    one_of('2', '3', '4', '8', '9')             >> split(3,4)   | # 1 digit ndc
+    match(/^(5[012345689]|7[234679])\d+$/)      >> split(3,4)     # 2 digit ndc
 
   country '973', none >> split(4,4..4) # Bahrain (Kingdom of) http://www.itu.int/oth/T0202000011/en
 
