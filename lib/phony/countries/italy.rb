dd9a88ac6241e89fd5c984813b5a29a9210e5adf
Florian R. Hanke <florian.hanke@gmail.com>
2023-01-23 19:17:14 +0100

    2.20.2

--- lib/phony/countries/italy.rb
+++ lib/phony/countries/italy.rb
@@ -256,7 +256,8 @@ ndcs_3digit = [
 # but this was not frozen so easily.
 mobile = %w(300 301 302 303 304 305 306 307 308 309 310 311 312 313 314 315 316 317 318 319 320 321 322 323 324 325 326 327 328 329 330 331 332 333 334 335 336 337 338 339 340 341 342 343 344 345 346 347 348 349 350 351 352 353 354 355 356 357 358 359 360 361 362 363 364 365 366 367 368 369 370 371 372 373 374 375 376 377 378 379 380 381 382 383 384 385 386 387 388 389 390 391 392 393 394 395 396 397 398 399)
 
-service = [ # Not exhaustive.
+# Not exhaustive.
+service = [
  '112',
  '113',
  '115',
@@ -268,7 +269,8 @@ service = [ # Not exhaustive.
  '1530'
 ]
 
-free_of_charge_services = [ # Not exhaustive.
+# Not exhaustive. Unused below - just for info purposes.
+free_of_charge_services = [
   '800',
   '803'
 ]
--- phony.gemspec
+++ phony.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'phony'
-  s.version = '2.20.1'
+  s.version = '2.20.2'
   s.authors = ['Florian Hanke']
   s.email = 'florian.hanke+phony@gmail.com'
   s.homepage = 'https://github.com/floere/phony'
