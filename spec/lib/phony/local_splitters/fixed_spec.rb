d3b2d67f01477a3c8146d8eab65f8d02aa747c02
Florian R. Hanke <florian.hanke@gmail.com>
2024-10-12 13:38:05 +0200

    Replace before with let.

--- spec/lib/phony/local_splitters/fixed_spec.rb
+++ spec/lib/phony/local_splitters/fixed_spec.rb
@@ -17,48 +17,42 @@ describe Phony::LocalSplitters::Fixed do
 
   describe 'split' do
     context 'without format' do
-      before do
-        @splitter = described_class.new
-      end
+      let(:splitter) { described_class.new }
 
       it 'splits correctly' do
-        @splitter.split('3643532').should == %w[364 35 32]
+        splitter.split('3643532').should == %w[364 35 32]
       end
 
       it 'splits correctly even when the number is too long (but leniently)' do
-        @splitter.split('3643532111').should == %w[364 35 32111]
+        splitter.split('3643532111').should == %w[364 35 32111]
       end
 
       it 'splits correctly even when the number is too short' do
-        @splitter.split('364353').should == %w[364 35 3]
+        splitter.split('364353').should == %w[364 35 3]
       end
     end
 
     context 'with format' do
-      before do
-        @splitter = described_class.new [3, 2, 2]
-      end
+      let(:splitter) { described_class.new([3, 2, 2]) }
 
       it 'splits correctly' do
-        @splitter.split('3643532').should == %w[364 35 32]
+        splitter.split('3643532').should == %w[364 35 32]
       end
 
       it 'splits correctly even when the number is too long (but leniently)' do
-        @splitter.split('3643532111').should == %w[364 35 32111]
+        splitter.split('3643532111').should == %w[364 35 32111]
       end
 
       it 'splits correctly even when the number is too short' do
-        @splitter.split('364353').should == %w[364 35 3]
+        splitter.split('364353').should == %w[364 35 3]
       end
     end
 
     context 'with hard number (iceland regression)' do
-      before do
-        @splitter = described_class.new [3, 4]
-      end
+      let(:splitter) { described_class.new([3, 4]) }
 
       it 'splits correctly' do
-        @splitter.split('112').should == ['112']
+        splitter.split('112').should == ['112']
       end
     end
   end
