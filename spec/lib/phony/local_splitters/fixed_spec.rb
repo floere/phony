fa43909b715ce87b4ba4837f4326f4ebaaec6ab4
Florian R. Hanke <florian.hanke@gmail.com>
2024-10-12 13:51:25 +0200

    Replace should -> expect.

--- spec/lib/phony/local_splitters/fixed_spec.rb
+++ spec/lib/phony/local_splitters/fixed_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Phony::LocalSplitters::Fixed do
   describe 'instance_for' do
     it 'caches' do
-      described_class.instance_for([3, 2, 2]).should equal(described_class.instance_for([3, 2, 2]))
+      expect(described_class.instance_for([3, 2, 2])).to equal(described_class.instance_for([3, 2, 2]))
     end
 
     it 'caches correctly' do
-      described_class.instance_for([1, 2, 3]).should_not equal(described_class.instance_for([9, 9, 9]))
+      expect(described_class.instance_for([1, 2, 3])).not_to equal(described_class.instance_for([9, 9, 9]))
     end
 
     it 'caches correctly' do
-      described_class.instance_for.should equal(described_class.instance_for)
+      expect(described_class.instance_for).to equal(described_class.instance_for)
     end
   end
 
@@ -20,15 +20,15 @@ describe Phony::LocalSplitters::Fixed do
       let(:splitter) { described_class.new }
 
       it 'splits correctly' do
-        splitter.split('3643532').should == %w[364 35 32]
+        expect(splitter.split('3643532')).to eq %w[364 35 32]
       end
 
       it 'splits correctly even when the number is too long (but leniently)' do
-        splitter.split('3643532111').should == %w[364 35 32111]
+        expect(splitter.split('3643532111')).to eq %w[364 35 32111]
       end
 
       it 'splits correctly even when the number is too short' do
-        splitter.split('364353').should == %w[364 35 3]
+        expect(splitter.split('364353')).to eq %w[364 35 3]
       end
     end
 
@@ -36,15 +36,15 @@ describe Phony::LocalSplitters::Fixed do
       let(:splitter) { described_class.new([3, 2, 2]) }
 
       it 'splits correctly' do
-        splitter.split('3643532').should == %w[364 35 32]
+        expect(splitter.split('3643532')).to eq %w[364 35 32]
       end
 
       it 'splits correctly even when the number is too long (but leniently)' do
-        splitter.split('3643532111').should == %w[364 35 32111]
+        expect(splitter.split('3643532111')).to eq %w[364 35 32111]
       end
 
       it 'splits correctly even when the number is too short' do
-        splitter.split('364353').should == %w[364 35 3]
+        expect(splitter.split('364353')).to eq %w[364 35 3]
       end
     end
 
@@ -52,7 +52,7 @@ describe Phony::LocalSplitters::Fixed do
       let(:splitter) { described_class.new([3, 4]) }
 
       it 'splits correctly' do
-        splitter.split('112').should == ['112']
+        expect(splitter.split('112')).to eq ['112']
       end
     end
   end
