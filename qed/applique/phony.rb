7c5c659cdb8ce1e5e0002a0fe5fbebf56cb43985
Florian R. Hanke <florian.hanke@gmail.com>
2024-10-12 12:32:17 +0200

    PerformanceRatio -> PERFORMANCE_RATIO.

--- qed/applique/phony.rb
+++ qed/applique/phony.rb
@@ -37,21 +37,21 @@ end
 # performance_of { do something here }.should < 0.0001
 #
 require 'benchmark'
-# Try to load the Phony::PerformanceRatio from
+# Try to load the Phony::PERFORMANCE_RATIO from
 # the file spec/performance_ratio.rb - set it to the
 # ratio you think it is compared to my MacBook 2.66GHz i7.
 #
 # Example:
-#   Phony::PerformanceRatio = 0.8 # for 80% of my MacBook speed.
+#   Phony::PERFORMANCE_RATIO = 0.8 # for 80% of my MacBook speed.
 #
 begin
   require File.expand_path 'performance_ratio', __dir__
 rescue LoadError
-  Phony::PerformanceRatio = 0 # Ignore speed tests by default.
+  Phony::PERFORMANCE_RATIO = 0 # Ignore speed tests by default.
 end
 def performance_of(&block)
   GC.disable
   result = Benchmark.realtime(&block)
   GC.enable
-  result * Phony::PerformanceRatio
+  result * Phony::PERFORMANCE_RATIO
 end
--- spec/spec_helper_extensions.rb
+++ spec/spec_helper_extensions.rb
@@ -2,23 +2,23 @@ module SpecHelperExtensions
   # performance_of { do something here }.should < 0.0001
   #
   require 'benchmark'
-  # Try to load the Phony::PerformanceRatio from
+  # Try to load the Phony::PERFORMANCE_RATIO from
   # the file spec/performance_ratio.rb - set it to the
   # ratio you think it is compared to my MacBook 2.66GHz i7.
   #
   # Example:
-  #   Phony::PerformanceRatio = 0.8 # for 80% of my MacBook speed.
+  #   Phony::PERFORMANCE_RATIO = 0.8 # for 80% of my MacBook speed.
   #
   begin
     require File.expand_path 'performance_ratio', __dir__
   rescue LoadError
     # Ignore speed tests by default.
-    Phony::PerformanceRatio = 0 unless Phony.constants.include?(:PerformanceRatio)
+    Phony::PERFORMANCE_RATIO = 0 unless Phony.constants.include?(:PERFORMANCE_RATIO)
   end
   def performance_of(&block)
     GC.disable
     result = Benchmark.realtime(&block)
     GC.enable
-    result * Phony::PerformanceRatio
+    result * Phony::PERFORMANCE_RATIO
   end
 end
